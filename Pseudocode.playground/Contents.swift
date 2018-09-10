56d2e1509e965f9d51cec4290da97b2139c3a63b
Janell Anderson <andersonjanell@gmail.com>
2018-09-10 18:27:33 -0400

    used pseudocode examples from the book for a better understanding of how to use it.

---
+++ Pseudocode.playground/Contents.swift
@@ -0,0 +1,58 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+/*
+ This are examples of Pseudocoding.
+ If you run this there will be errors
+*/
+
+                                // EXAMPLE FOR isComplete AS TRUE
+x = 5
+y = 6
+isComplete = TRUE
+
+if x < y {
+//    in this example, x is less than 6
+    do stuff
+}
+else {
+    do other stuff
+}
+
+// EXAMPLE 2
+if isComplete == TRUE {
+//    in this example, isComplete is equal to TRUE
+    do stuff
+}
+else {
+    do other stuff
+}
+
+// another way to check isComplete == TRUE
+if isComplete {
+//    in this example, is complete is TRUE
+    do stuff
+}
+
+                                // EXAMPLE FOR isComplete AS FALSE
+
+// two ways to check is a value is false
+if isComplete {
+    do stuff
+}
+else {
+//    in this example, isComplete is TRUE so the else block with be executed
+    do stuff
+}
+
+// another way to check isComplete == FALSE
+if !isComplete {
+    do stuff
+}
+else {
+//    in this example, isComplete is TRUE so the else block will be executed
+    do other stuff
+}
