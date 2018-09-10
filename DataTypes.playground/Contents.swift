612e22f804768d04892759e910e4a8947979eeb4
Janell Anderson <andersonjanell@gmail.com>
2018-09-10 15:53:27 -0400

    Decided to look at Swift 4 coding for iOS development.

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,35 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+// Tutorial code
+// Declaring firstNumber and secondNumber
+var firstNumber = 2
+var secondNumber = 3
+
+var totalSum = firstNumber + secondNumber
+
+firstNumber = firstNumber + 1
+secondNumber = secondNumber + 1
+
+totalSum = firstNumber + secondNumber
+
+print("totalSum = \(totalSum)")
+
+//Personal Code
+//Multiples two integers and displays the result
+var thirdNumber = 3
+var fourthNumber = 4
+
+var totalNum = thirdNumber * fourthNumber
+
+print("totalNum = \(totalNum)")
+
+//Squares a float. Display the resulting float
+var num1 = 36.666
+
+var totalSqrt = sqrt(num1)
+
+print("totalSqrt = \(totalSqrt)")
---
+++ HelloWorld.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground."
+var new = " How are you?"
+print(str + new)
+
+var name = "My name is Janell"
+print(name)
+
