f63871355eaf1e92caa9f7501fbba7e43ad8a704
Janell Anderson <andersonjanell@gmail.com>
2018-09-10 18:36:42 -0400

    Finished the example used for if-then-else using conditional operators and logical operators

--- Pseudocode.playground/Contents.swift
+++ Pseudocode.playground/Contents.swift
@@ -9,6 +9,8 @@ var str = "Hello, playground"
  If you run this there will be errors
 */
 
+// Using Conditional in if-then-else Code
+
                                 // EXAMPLE FOR isComplete AS TRUE
 x = 5
 y = 6
@@ -54,5 +56,39 @@ if !isComplete {
 }
 else {
 //    in this example, isComplete is TRUE so the else block will be executed
+    do other stuff
+}
+
+// Using && and || Logical Operators
+
+x = 5
+y = 6
+isComplete = TRUE
+
+// using the logical AND
+if x < y && isComplete == TRUE {
+//    in this example, x is less than 6 and isComplete == TRUE
+    do stuff
+}
+
+if x < y || isComplete == FALSE {
+//    in this example, x is less than 6.
+//    Only one operand has to be TRUE for an OR to result in a TRUE
+//    see OR truth table
+    do stuff
+}
+
+// another way to test for TRUE
+if x < y && isComplete {
+//    in this example, x is less than 6 and isComplete == TRUE
+    do stuff
+}
+
+// another way to test for FALSE
+if x < y && !isComplete {
+    do stuff
+}
+else {
+//    isComplete == TRUE
     do other stuff
 }
